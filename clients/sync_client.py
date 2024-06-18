3f187a5513ac2c4c27d5706062a361dead2eeb7d
DTheIcyDragon <74721808+DTheIcyDragon@users.noreply.github.com>
2024-06-18 18:44:47 +0200

    Update sync_client.py
    Fix SSL Error

--- clients/sync_client.py
+++ clients/sync_client.py
@@ -1,4 +1,7 @@
 import requests
+import urllib3
+
+urllib3.disable_warnings()  # to suppress the `verify=False` statement in every request. best I've found so far 
 
 
 class RedirectRequest:
@@ -6,11 +9,12 @@ class RedirectRequest:
         self.url = url
         self.session = session
         self.secret = secret
+        self.headers = {"User-agent": "Dragons_RedirectWrapper 1.0.1"}
 
-    def get_redirect(self, redirect_name: str):
-        return self.session.get(f"{self.url}/api/get_redirect/{redirect_name}")
+    def get_redirect(self, redirect_name: str, allow_redirects: bool = True):
+        return self.session.get(f"{self.url}/api/get_redirect/{redirect_name}", verify=False, headers=self.headers)
 
-    def add_redirect(self, redirect_name: str, redirect_value: str):
+    def add_redirect(self, redirect_name: str, redirect_value: str, allow_redirects: bool = True):
         return self.session.post(
             f"{self.url}/api/add_redirect",
             json={
@@ -18,9 +22,11 @@ class RedirectRequest:
                 "value": redirect_value,
                 "secret": self.secret,
             },
+            verify=False,
+            headers=self.headers,
         )
 
-    def update_redirect(self, redirect_name: str, redirect_value: str):
+    def update_redirect(self, redirect_name: str, redirect_value: str, allow_redirects: bool = True):
         return self.session.put(
             f"{self.url}/api/update_redirect/{redirect_name}",
             json={
@@ -28,18 +34,21 @@ class RedirectRequest:
                 "value": redirect_value,
                 "secret": self.secret,
             },
+            verify=False,
+            headers=self.headers,
         )
 
-    def remove_redirect(self, redirect_name: str):
+    def remove_redirect(self, redirect_name: str, allow_redirects: bool = True):
         return self.session.delete(
             f"{self.url}/api/remove_redirect",
             json={"name": redirect_name, "value": "", "secret": self.secret},
+            verify=False,
+            headers=self.headers,
         )
 
 
 if __name__ == "__main__":
-    main = RedirectRequest("http://127.0.0.1:8000", requests.Session(), "SOME_SUPER_SECURE_SECRET")
-
+    main = RedirectRequest("https://your.redirect.example.com", requests.Session(), "SOME_SUPER_SECURE_SECRET")
     assert main.add_redirect("test", "https://127.0.0.1:8000/super_secret_route").status_code == 200
     assert main.update_redirect("test", "https://127.0.0.1:8000/test").status_code == 200
     assert main.get_redirect("test").status_code == 200
